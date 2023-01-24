a1e84f587c5f204b3b91781e9277ee8b03e83d9f
Henrique Rutter <hfrutter@hotmail.com>
2023-01-24 08:20:17 -0300

    Adicionar arquivos de projeto.

---
+++ ContaCorrente.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public int numero_agencia;
+        public string? conta_corrente;
+        public string? titular;
+        public double saldo;
+
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+            Console.WriteLine("O valor: R$" + valor + " foi depositado com sucesso a sua conta!");
+            Console.WriteLine("Saldo atual: R$" + this.saldo);
+            Console.WriteLine("======================");
+
+        }
+
+        public bool Sacar(double valor) 
+        {
+            if (valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                Console.WriteLine("O saque no valor de: R$" + valor + " foi sacado com sucesso, aguarde a liberação das notas. O saldo atual é de: R$" + this.saldo);
+                Console.WriteLine("======================");
+                return true;
+                
+            }
+            else
+            {
+                Console.WriteLine("O valor de saque é superior ao saldo que se possui na conta, você não tem saldo para completar essa transação.");
+                Console.WriteLine("======================");
+                return false;
+            }
+
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino) 
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.Sacar(valor);
+                destino.Depositar(valor);
+                Console.WriteLine(this.titular + " transferiu: R$ " + valor + " para: " + destino.titular + ". O Saldo restante é de: R$ " + this.saldo);
+                Console.WriteLine("======================");
+                return true;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using bytebank;
+using System.Runtime.CompilerServices;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+
+contaDoAndre.titular = "André";
+contaDoAndre.saldo = 1000.00;
+contaDoAndre.numero_agencia = 0119;
+contaDoAndre.conta_corrente = "47835-7";
+
+Console.WriteLine("Foram solicitadas as informações de um cliente ao terminal, enviando-as.");
+
+
+
+Console.WriteLine("Titular da conta: " + contaDoAndre.titular);
+Console.WriteLine("Saldo disponivel na conta: " + contaDoAndre.saldo);
+Console.WriteLine("Número da agencia: " + contaDoAndre.numero_agencia);
+Console.WriteLine("Número da Conta Corrente: " + contaDoAndre.conta_corrente);
+
+Console.WriteLine("======================");
+
+
+contaDoAndre.Depositar(10000.00);
+contaDoAndre.Depositar(5412.00);
+contaDoAndre.Sacar(1000.00);
+
+ContaCorrente contaDaEtnilda= new ContaCorrente();
+
+contaDaEtnilda.titular = "Jéssica";
+contaDaEtnilda.conta_corrente = "77777-51";
+contaDaEtnilda.numero_agencia = 051;
+contaDaEtnilda.saldo = 10000;
+
+contaDoAndre.Transferir(1500.00, contaDaEtnilda);
